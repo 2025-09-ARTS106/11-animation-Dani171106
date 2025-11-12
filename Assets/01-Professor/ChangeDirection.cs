59a9c2eb917949e7cc07d4e7b47967630c4de437
Dani171106 <G00319065@stu.raritanval.edu>
2025-11-12 13:19:41 -0500

    SIGHHHHHH

--- Assets/01-Professor/ChangeDirection.cs
+++ Assets/01-Professor/ChangeDirection.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class ChangeDirection : MonoBehaviour
 {
+
+    private Animator anim;
+    private float horizontalMove;
+    private float verticalMove;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+anim=GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+
+        if (horizontalMove < 0)
+        {
+            anim.SetInteger("Direction", 3);
+        }
+        if (horizontalMove > 0)
+        {
+            anim.SetInteger("Direction", 1);
+        }
+
+
+        if (verticalMove < 0)
+        {
+            anim.SetInteger("Direction", 2);
+        }
+        if (verticalMove > 0)
+        {
+            anim.SetInteger("Direction", 0);
         
+        }
     }
 }
+
+
